fd6c15679d53c180ed3d5d9fcf9d8d4940da4aa2
Simon <simonrosenberg1@gmail.com>
2021-09-07 14:20:07 +0100

    Test: Add test for normal balls and initial strike

--- src/main/java/Bowling.java
+++ src/main/java/Bowling.java
@@ -1,2 +1,30 @@
 public class Bowling {
+
+    public int calculateScore(String results) {
+
+        int score = 0;
+        int resultsIndex = 0;
+
+        String[] characters = results.replace("|", "").split("");
+        for(String c : characters) {
+            if (c.matches("[0-9]")) {
+                score += Integer.parseInt(c);
+            }
+            if (c.matches("[X]")) {
+                score += 10;
+                if (characters[resultsIndex + 1].matches("[0-9X]")) {
+                    if (characters[resultsIndex + 1].matches("[X]")) {
+                        score += 10;
+                    } else {
+                        score += results.charAt(resultsIndex + 1);
+                    }
+                }
+                if (characters[resultsIndex + 2].matches("[0-9X]")) {
+                    score += results.charAt(resultsIndex + 2);
+                }
+            }
+            resultsIndex++;
+        }
+        return score;
+    }
 }
--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -1,2 +1,35 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BowlingTest {
+
+    @Test
+    public void shouldReturn0WhenAllFramesAreMisses() {
+        Bowling bowling = new Bowling();
+        assertEquals(0, bowling.calculateScore("--|--|--|--|--|--|--|--|--|--||"));
+    }
+
+    @Test
+    public void shouldReturn10WhenAllFramesScore1() {
+        Bowling bowling = new Bowling();
+        assertEquals(10, bowling.calculateScore("1-|1-|1-|1-|1-|1-|1-|1-|1-|1-||"));
+    }
+
+    @Test
+    public void shouldReturn15WhenFramesAreHalf1Half2() {
+        Bowling bowling = new Bowling();
+        assertEquals(15, bowling.calculateScore("1-|1-|1-|1-|1-|2-|2-|2-|2-|2-||"));
+    }
+
+    @Test
+    public void shouldReturn20WhenFirstFrameIsStrikeFollowedBy1ForRemainingFrames() {
+        Bowling bowling = new Bowling();
+        assertEquals(20, bowling.calculateScore("X|1-|1-|1-|1-|1-|1-|1-|1-|1-||"));
+    }
+
+    @Test
+    public void shouldReturn20WhenFirstTwoFramesAreStrikesFollowedBy1ForRemainingFrames() {
+        Bowling bowling = new Bowling();
+        assertEquals(40, bowling.calculateScore("X|X|1-|1-|1-|1-|1-|1-|1-|1-||"));
+    }
 }
